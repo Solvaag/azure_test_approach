03c1dc5ff14a4719a6df6b3c43aac8f499479d7b
Sigve Solvaag <sybaronde@gmail.com>
2021-11-02 14:19:19 +0100

    Made a file copy solution.

---
+++ sdir_config.py
@@ -0,0 +1,8 @@
+RESOURCE_GROUP = "sdir-d-rg-risk"
+
+DATA_FACTORY = "sdir-d-adf-risk"
+
+CALL_SIGNS = ["LFJJ", "JWVX", "LCAM", "LGBA", "LCBK", "LCQU", "3YSE", "LNFF", "LFRD", "LEGL", "JWLV", "LNHP", "LNPV", "LIOB",
+     "LFVT", "LECJ", "LJEU", "LCCK", "LF5243", "LDNL", "LEDF", "LAIK", "LNNM", "LCBX", "JWLP", "LCBD", "LIWT", "LFIS",
+     "LATP", "LFIK", "LKFP", "LK5993", "JWMA", "LGLK", "3YWR", "3YZA", "LGJL", "LDPZ", "LDWH", "LFFD", "LCJH", "JWMF",
+     "JWNM", "LDHI", "JXQF", "LAPB", "LEDI", "LCNQ", "LLWW", "LCJY"]
--- testbed.py
+++ testbed.py
@@ -1,5 +1,15 @@
 from azure.mgmt.datafactory import DataFactoryManagementClient
 from azure.mgmt.resource.resources import ResourceManagementClient
+from azure.mgmt.datafactory.models import DatasetResource, AzureBlobDataset, \
+    DatasetReference, BlobSink, BlobSource, PipelineResource, CopyActivity, \
+    LinkedServiceResource, AzureStorageLinkedService, LinkedServiceReference
+
+import os
+
+import time
+from datetime import datetime
+
+from sdir_config import RESOURCE_GROUP, DATA_FACTORY
 
 # from azure.common.credentials import ServicePrincipalCredentials  # To login with service principal (appid and client secret) use this
 from azure.identity import InteractiveBrowserCredential
@@ -26,6 +36,14 @@ def developer_validation(file='creds.json'):
     return data["id"], data["tenantId"]
 
 
+def load_configuration(file='parameters.json'):
+
+    with open(file, 'r') as parameter_file:
+        parameters = json.load(parameter_file)
+
+    return parameters
+
+
 def get_subscriptions():
     subscriptions = json.loads(subprocess.check_output('az account list', shell=True).decode('utf-8'))
 
@@ -46,6 +64,35 @@ def get_resources_in_group(resource_group, credentials, subscription_id):
     return resources
 
 
+def piperun_by_callsign(call_sign=None, pipeline_name=None):
+
+    subscription_id, tenant_id = developer_validation()
+
+    credentials = InteractiveBrowserCredential(tenant_id=tenant_id)
+
+    adf_client = DataFactoryManagementClient(credentials, subscription_id)
+
+    parameters = {
+        'call_sign': call_sign
+    }
+
+    run_response = adf_client.pipelines.create_run(RESOURCE_GROUP, DATA_FACTORY, pipeline_name, parameters=parameters)
+
+    now = datetime.now()
+
+    while True:
+        time.sleep(30)
+        runtime = datetime.now()
+        duration = runtime - now
+        print("Duration {} seconds".format(duration.seconds))
+
+        pipeline_run = adf_client.pipeline_runs.get(RESOURCE_GROUP, DATA_FACTORY, run_response.run_id)
+
+        print("\n\tPipeline run status: {}".format(pipeline_run.status))
+
+
+
+
 def trigger_run():
     subscription_id, tenant_id = developer_validation()
 
@@ -61,19 +108,96 @@ def trigger_run():
 
     adf_client = DataFactoryManagementClient(credentials, subscription_id)
 
-    rg_name = "sdir-d-rg-risk"
-    df_name = "sdir-d-adf-risk"
+    doof = {
+        # 'indicators': '["callsToPortPerDistanceTraveled","portCalls","avgVelocity"]',
+        'call_sign': 'JXNC',
+    }
 
-    p_name = "prepare_data"
+    # foo = adf_client.pipelines.create_run(rg_name, df_name, p_name, parameters=doof)
 
-    # foo = adf_client.pipelines.create_run(rg_name, df_name, p_name)
+    # https://sdirdstdataadfrisk00.blob.core.windows.net/sandbox/source/help_i_am_a_textfile.txt
 
-    foo = get_resources_in_group(rg_name, credentials, subscription_id)
+    connstring = ""
 
-    print(foo)
-    for thing in foo:
-        print(thing)
+    ls_name = 'storageLinkedService001'
+    ls_azure_storage = LinkedServiceResource(properties=AzureStorageLinkedService(connection_string=connstring))
+    ls = adf_client.linked_services.create_or_update(RESOURCE_GROUP, DATA_FACTORY, ls_name, ls_azure_storage)
+    ds_ls = LinkedServiceReference(reference_name=ls_name)
+
+    source_set = create_dataset('sandbox', 'source', 'help_i_am_a_textfile.txt', ds_ls)
+    source_name = "foo"
+
+    source_output = adf_client.datasets.create_or_update(RESOURCE_GROUP, DATA_FACTORY, source_name, source_set)
+
+    print(source_output)
+
+    sink_set = create_dataset('sandbox', 'sink', 'help_i_am_a_textfile.txt', ds_ls)
+    sink_name = "bar"
+
+    sink_output = adf_client.datasets.create_or_update(RESOURCE_GROUP, DATA_FACTORY, sink_name, sink_set)
+
+    print(sink_output)
+
+    pipename, pipeobj = copy_results(source_name, sink_name)
+
+    factory_output = adf_client.pipelines.create_or_update(RESOURCE_GROUP, DATA_FACTORY, pipename, pipeobj)
+
+    print(factory_output)
+
+    run_response = adf_client.pipelines.create_run(RESOURCE_GROUP, DATA_FACTORY, pipename, parameters={})
+
+    print(run_response)
+
+    # foo = get_resources_in_group(rg_name, credentials, subscription_id)
+    #
+    # res_client = ResourceManagementClient(credentials, subscription_id)
+    #
+    #
+    #
+    # print(foo)
+    # for thing in foo:
+    #     print(thing)
+
+
+def create_dataset(container, folder_path, filename, linked_service=None):
+
+    blob_path = os.path.join(container, folder_path)
+    blob_filename = filename
+
+    ds_azure_blob = DatasetResource(properties=AzureBlobDataset(
+        linked_service_name=linked_service, folder_path=blob_path, file_name=blob_filename))
+
+    return ds_azure_blob  # must still be updated in client
+
+
+def copy_results(source_set_name, sink_set_name):
+    # Create a copy activity
+    act_name = 'copyResults'
+    blob_source = BlobSource()
+    blob_sink = BlobSink()
+    dsin_ref = DatasetReference(reference_name=source_set_name)
+    dsOut_ref = DatasetReference(reference_name=sink_set_name)
+    copy_activity = CopyActivity(name=act_name, inputs=[dsin_ref], outputs=[
+        dsOut_ref], source=blob_source, sink=blob_sink)
+
+    # Create a pipeline with the copy activity
+    pipeline_name = 'copyPipeline'
+    params_for_pipeline = {}
+    pipeline_object = PipelineResource(
+        activities=[copy_activity], parameters=params_for_pipeline)
+
+    return pipeline_name, pipeline_object
+
+
+def main():
+
+    basecase = "LFJJ"
+
+    pipeline = "risk_module"
+
+    piperun_by_callsign(basecase, pipeline)
+
 
 
 if __name__ == '__main__':
-    trigger_run()
+    main()
